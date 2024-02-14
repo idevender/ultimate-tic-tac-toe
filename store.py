52cd135a787fcced814d3bec6d0fcb6c3c59f4dd
evan-best <117332327+evan-best@users.noreply.github.com>
2024-02-13 21:33:54 -0330

    added methods for user storage.

--- store.py
+++ store.py
@@ -1,7 +1,9 @@
 import shelve
 
 
-def setUp():
+# GAME STATE METHODS
+
+def setup_game_db():
     """Creates the database for game states.
 
     Returns:
@@ -13,7 +15,7 @@ def setUp():
         return len(db)
 
 
-def save(name):
+def save_game(name):
     """Saves a game state to the database.
 
     THIS IS A STUB
@@ -27,7 +29,7 @@ def save(name):
     pass
 
 
-def load(name):
+def load_game(name):
     """Loads a previously stored game state from the database.
     
     THIS IS A STUB
@@ -41,11 +43,11 @@ def load(name):
     pass
 
 
-def remove(name):
+def remove_game(name):
     """Removes a previously stored game state from the database.
 
     THIS IS A STUB
-    
+
     Params:
         name (str): The name of the game state to be removed.
     
@@ -53,3 +55,59 @@ def remove(name):
         IOError: If the game state cannot be destroyed.
     """
     pass
+
+
+# USER INFORMATION METHODS
+
+def setup_user_db():
+    """Creates the database for user information.
+
+    Returns:
+        int: the number of users in the database.
+    
+        
+    """
+    with shelve.open('users') as db:
+        return len(db)
+    
+def save_user(user_name):
+    """Saves a user to the database.
+
+    THIS IS A STUB
+
+    Params:
+        user_name (str): Assign a name to the newly stored user.
+
+    Raises:
+        Standard IO errors if the user cannot be saved properly.
+    """
+    pass
+
+
+def load_user(user_name):
+    """Loads a previously stored user from the database.
+    
+    THIS IS A STUB
+
+    Params:
+        user_name (str): The name of the user to be loaded.
+
+    Raises:
+        IOError: If the user state cannot be loaded properly.
+    """
+    pass
+
+
+def remove_user(user_name):
+    """Removes a previously stored user from the database.
+
+    THIS IS A STUB
+
+    Params:
+        user_name (str): The name of the user to be removed.
+    
+    Raises:
+        IOError: If the user cannot be destroyed.
+    """
+    pass
+
