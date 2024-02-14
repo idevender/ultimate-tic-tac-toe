521ed76eebf56a7fd862b9f57512c0f2e49ba64e
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2024-02-13 20:45:28 -0330

    unittests upload

--- serverAPI.py
+++ serverAPI.py
@@ -1,10 +1,12 @@
+
+# Imports
 import webbrowser
 from bottle import Bottle, run, request, response, route, static_file, template
 
-
-
+#Create the bottle app
 app = Bottle()
 
+# Routes for loading pages
 @app.route('/')
 def get_homepage():
     """ This function returns the homepage of the server.
@@ -14,21 +16,23 @@ def get_homepage():
     """
     return "Hello World!"
 
-@app.route('/user')
-def verify_user():
-    """ This function verifies the user sign in information.
-
-    Returns:
-        Int: 404 if the user is not found, 200 if the user is found. 
-    """
-    return ("<h1> User Info </h1>")
-
 @app.route('/user/<user_id>')
 def get_user_page():
     """ This function returns the user's page.
 
     Returns:
         String: The user's page.
+    """
+    return ("<h1> User Info </h1>")
+
+
+# Routes for handling user information and authentication
+@app.route('/user')
+def verify_user():
+    """ This function verifies the user sign in information.
+
+    Returns:
+        Int: 404 if the user is not found, 200 if the user is found. 
     """
     return ("<h1> User Info </h1>")
 
@@ -42,17 +46,29 @@ def update_user_info():
     """
     return 404 | 200
 
+# Routes for handling game information
 @app.route('/load_game/<game_id>')
 def get_game_state():
-    pass
+    """ This function returns the game state of the given ID in JSON format.
+
+    Returns:
+        JSON: Returns the game state in JSON format.
+    """
+    
+    return 404 | 200
 
 @app.route('/save_game/<game_id>', method='POST')
 def update_game_state():
-    pass
+    """ This function updates the game state of the given ID.
+
+    Returns:
+        Int: 404 if the game is not found, 200 if the game is found.
+    """
+    
+    return 404 | 200
 
 
-
-
+#Starts the server and opens the web browser
 if __name__ == '__main__':
     run(app, host='localhost', port=8080)
     webbrowser.open('http://localhost:8080')
